e9a6d512baa26dbb1fa1e34b800bbe593bd68239
brMonteiro-G <brgabriel.monteiro@gmail.com>
2023-09-29 21:24:29 -0300

    feat(Challenge-3): add aws integration

---
+++ Challenge-3/src/main/java/com/ada/challenges/challenge/backend/aws/NotificationService.java
@@ -0,0 +1,35 @@
+package com.ada.challenges.challenge.backend.aws;
+
+import com.ada.challenges.challenge.backend.LegalPerson.LegalPerson;
+import com.ada.challenges.challenge.backend.NaturalPerson.NaturalPerson;
+import io.awspring.cloud.sns.core.SnsTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationService {
+
+
+    private final SnsTemplate snsTemplate;
+    private final String topicArn;
+
+
+    public NotificationService(SnsTemplate snsTemplate, @Value("${aws.topic-arn}") String topicArn) {
+        this.snsTemplate = snsTemplate;
+        this.topicArn = topicArn;
+    }
+
+    public void sendNotification(LegalPerson legalPerson){
+        snsTemplate.sendNotification(topicArn, legalPerson, "New message: Legal Person added");
+    }
+
+    public void sendNotification(NaturalPerson naturalPerson){
+        snsTemplate.sendNotification(topicArn, naturalPerson, "New message: Legal Person added");
+
+    }
+
+
+
+    
+
+}
---
+++ Challenge-3/src/main/java/com/ada/challenges/challenge/backend/aws/SqsConfiguration.java
@@ -0,0 +1,47 @@
+package com.ada.challenges.challenge.backend.aws;
+
+import com.ada.challenges.challenge.backend.LegalPerson.LegalPerson;
+import io.awspring.cloud.sqs.listener.SqsMessageListenerContainer;
+import io.awspring.cloud.sqs.operations.SqsTemplate;
+import lombok.Getter;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.services.sqs.SqsAsyncClient;
+
+import java.util.Arrays;
+
+@Log4j2
+@Getter
+@Configuration
+public class SqsConfiguration {
+
+    @Value("${aws.queue-name}")
+    private String queueName;
+    private String payloadMessage;
+    private String queueStatus;
+
+
+    @Bean
+    public SqsTemplate sqsTemplateConfig(SqsAsyncClient sqsAsyncClient){
+        return SqsTemplate.builder().sqsAsyncClient(sqsAsyncClient).build();
+    }
+
+    @Bean
+   public SqsMessageListenerContainer<LegalPerson> receiveMessage(SqsAsyncClient sqsAsyncClient){
+        return new SqsMessageListenerContainer.Builder<LegalPerson>()
+                .sqsAsyncClient(sqsAsyncClient).queueNames(this.queueName)
+                .messageListener((message -> this.payloadMessage = String.valueOf(message.getPayload()))).build();
+    }
+
+    @Bean
+    public SqsMessageListenerContainer.Builder<LegalPerson[]> receiveManyMessages(SqsAsyncClient sqsAsyncClient){
+        return new SqsMessageListenerContainer.Builder<LegalPerson[]>()
+                .sqsAsyncClient(sqsAsyncClient).queueNames(this.queueName).messageListener(message -> this.queueStatus = Arrays.toString(message.getPayload()));
+    }
+
+
+
+
+}
